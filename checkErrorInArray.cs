4ca6a69e8ca2718ac5c1189c5d81a829b495704f
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-09-10 15:46:03 +0200

    add system to check if order id has correct length

---
+++ checkErrorInArray.cs
@@ -0,0 +1,12 @@
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orders = orderStream.Split(',');
+Array.Sort(orders);
+
+for (int i = 0; i < orders.Length; i++)
+{
+  if (orders[i].Length != 4)
+  {
+    orders[i] += "\t- Error";
+  }
+    Console.WriteLine(orders[i]);
+}
