1c7ca65e60b77b2e90298257eaa86666d30211e6
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-09-10 15:47:39 +0200

    create method for coinflip

--- coinFlip.cs
+++ coinFlip.cs
@@ -1,21 +1,25 @@
-// // coin flip program
-// Random Random = new Random();
-// int coinFlip = Random.Next(0, 2);
-// int coinFlips = 10;
+// static void coinFlip()
+//     {
+//       Random Random = new Random();
+//       int coinFlip = Random.Next(0, 2);
+//       int coinFlips = 10;
 
-// int countHeads = 0;
-// int countTails = 0;
+//       int countHeads = 0;
+//       int countTails = 0;
 
-// for (int i = 0; i < coinFlips; i++)
-// {
-//   coinFlip = Random.Next(0, 2);
-//   if (coinFlip == 0) {
-//     Console.WriteLine("Heads");
-//     countHeads++;
-//   } else {
-//     Console.WriteLine("Tails");
-//     countTails++;
-//   }
-// }
-
-// Console.WriteLine($"Heads: {countHeads}, Tails: {countTails}");
+//       for (int i = 0; i < coinFlips; i++)
+//       {
+//         coinFlip = Random.Next(0, 2);
+//         if (coinFlip == 0)
+//         {
+//           Console.WriteLine("Heads");
+//           countHeads++;
+//         }
+//         else
+//         {
+//           Console.WriteLine("Tails");
+//           countTails++;
+//         }
+//       }
+//       Console.WriteLine($"Heads: {countHeads}, Tails: {countTails}");
+//     }
